e26ca73b6fbcd28ef3c26ced394958ae4a40d69a
Dmitriy B. <dbkkll@yandex.ru>
2024-01-28 12:12:08 +0500

    Add plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,7 +5,18 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.pack.rust" },
+
   { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.markdown-and-latex.vimtex" },
+
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.just" },
+
+
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
 }
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      "lua", "cpp", "c_sharp", "rust", "gitignore", "gitcommit", "git_rebase", "git_config", 
+      "lua", "cpp", "c_sharp", "rust", "toml", "gitignore", "gitcommit", "git_rebase", "git_config",
     })
   end,
 }
