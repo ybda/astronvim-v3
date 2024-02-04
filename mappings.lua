16ee90b47af7511fc62c115d2dbb82124f805f0e
Dmitriy B. <dbkkll@yandex.ru>
2024-02-04 09:45:52 +0500

    Add `Close Previous Buffer` key map

--- mappings.lua
+++ mappings.lua
@@ -32,6 +32,14 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+    ["<leader>bk"] = {
+      function()
+        require("astronvim.utils.buffer").prev()
+        require("astronvim.utils.buffer").close()
+      end,
+      desc = "Close Previous Buffer",
+    },
   },
   t = {
     -- setting a mapping to false will disable it
